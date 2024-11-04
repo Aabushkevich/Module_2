2a7db041a601ee7d30dc5aa92455e531fef65277
Aabushkevich <abushkevich9063@gmail.com>
2024-11-04 23:25:29 +0700

    Add files via upload

---
+++ modul_2_Hard.py
@@ -0,0 +1,14 @@
+number = int(input("Введите число от 3 до 20:" ))
+if 3 <= number <= 20:
+    res = []
+
+    for i in range(1, number):
+        for j in range(2, number):
+            if number % (i + j) == 0 and i != j:
+                res.extend([i, j])
+
+    res = ("".join(map(str, res)))
+    print(f"{number} - {res}")
+
+else:
+    print("Число вне указанного диапазона")
