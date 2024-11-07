38d7634499b33c44b54b4a3809ede05498afa272
Aabushkevich <abushkevich9063@gmail.com>
2024-11-07 19:45:44 +0700

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
 a = 0
-while  a <= len(my_list) and int(my_list[a]) >= 0 :
+my_list.remove(0)
+
+while a <= len(my_list):
     print(my_list[a])
     a += 1
-else:
-    print("")
 
+    if int(my_list[a]) < 0:
+        break
 
